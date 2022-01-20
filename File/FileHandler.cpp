4738f12605261ed93edcc3955b57699e36c2e42d
Leonid Tkachenko <leon24rus@gmail.com>
2022-01-20 22:49:18 +0700

    namespace file

--- File/FileHandler.cpp
+++ File/FileHandler.cpp
@@ -5,13 +5,13 @@
 #include "../BufferHandler.h"
 #include "../FileChunkDispencer.h"
 
-namespace WAL
+namespace WAL::File
 {
     FileHandler::FileHandler(IFile* file) : file(file)
     {
     }
 
-    std::vector<uint8_t> FileHandler::GetNextChunk(size_t size, bool& outIsEnd)
+    std::vector<uint8_t> FileHandler::GetNextChunk(size_t size, bool& outIsNextExist)
     {
         if (file == nullptr) return std::vector<uint8_t>(0);
         
@@ -21,7 +21,7 @@ namespace WAL
 
         bool isComplete = false;
         data = dispenser.GetNextChunk(size, isComplete);
-        if (!isComplete) outIsEnd = true; else outIsEnd = false;
+        if (!isComplete) outIsNextExist = true; else outIsNextExist = false;
         return data;
 
     }
--- File/FileHandler.h
+++ File/FileHandler.h
@@ -7,26 +7,29 @@
 
 namespace WAL
 {
-	//Forwarding
-	class IFile;
-
-	class FileHandler final
+	namespace File
 	{
-	private:
-		size_t currentPos{ 0 };
-		//size_t handledSize{ 0 };
-		IFile* file{ nullptr };
+		//Forwarding
+		class IFile;
 
-	public:
-		FileHandler() = delete;
-		FileHandler(IFile* file);
+		class FileHandler final
+		{
+		private:
+			size_t currentPos{ 0 };
+			//size_t handledSize{ 0 };
+			IFile* file{ nullptr };
+
+		public:
+			FileHandler() = delete;
+			FileHandler(IFile* file);
 
 
-		/**
-		* Returns chunk of buffer for extracting pixels. Size of chunk must be precalculated for rows being filled without empty bytes at the end
-		* @param size
-		*/
-		std::vector<uint8_t> GetNextChunk(size_t size, bool &outIsEnd);
+			/**
+			* Returns chunk of buffer for extracting pixels. Size of chunk must be precalculated for rows being filled without empty bytes at the end
+			* @param size
+			*/
+			std::vector<uint8_t> GetNextChunk(size_t size, bool& outIsNextExist);
 
-	};
+		};
+	}
 }
