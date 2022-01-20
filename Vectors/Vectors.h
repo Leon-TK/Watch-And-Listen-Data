ea7b0c0c84fb49f363bf29003074fce882e48b7f
Leonid Tkachenko <leon24rus@gmail.com>
2022-01-20 11:55:23 +0700

    added template vectors

--- Vectors/Vectors.h
+++ Vectors/Vectors.h
@@ -3,23 +3,34 @@
 */
 namespace WAL
 {
-	template <typename T>
-	class Vec2
+	namespace Vectors
 	{
-	public:
-		Vec2() = default;
-		Vec2(T x, T y) : X(x), Y(y) {};
-		
-		T X;
-		T Y;
-
-		const Vec2 operator=(const Vec2& rhs)
+		template<typename T>
+		struct TVec2
 		{
-			Vec2 New;
-			New.X = rhs.X;
-			New.Y = rhs.Y;
+			TVec2() : x(0), y(0) {};
+			TVec2(const T x, const T y) : x(x), y(y) {};
+			T x;
+			T y;
 
-			return New;
-		}
-	};
+			const TVec2 operator=(const TVec2& rhs)
+			{
+				return TVec2(rhs.x, rhs.y);
+			}
+		};
+		template<typename T>
+		struct TVec3
+		{
+			TVec3() : x(0), y(0), z(0) {};
+			TVec3(const T x, const T y, const T z) : x(x), y(y), z(z) {};
+			T x;
+			T y;
+			T z;
+
+			const TVec3 operator=(const TVec3& rhs)
+			{
+				return TVec3(rhs.x, rhs.y, ths.z);
+			}
+		};
+	}
 }
