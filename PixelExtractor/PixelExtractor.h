20297f685dd779efe8238d86b7b842d1e3cd53ac
Leonid Tkachenko <leon24rus@gmail.com>
2022-01-20 22:23:08 +0700

    Move pixels in separate file

--- PixelExtractor/PixelExtractor.h
+++ PixelExtractor/PixelExtractor.h
@@ -9,41 +9,7 @@
 */    
 namespace WAL
 {
-	namespace Pixels
-	{
-		template <typename ChannelType>
-		class TRgbPixel final
-		{
-			Vectors::TVec3<ChannelType> channels;
-		};
-		template <typename ChannelType>
-		class TRgbaPixel final
-		{
-			Vectors::TVec3<ChannelType> channels;
-			ChannelType alpha;
-		};
-		template <typename ChannelType>
-		class TMonoPixel final
-		{
-			ChannelType channel;
-		};
-
-		class RgbPixel final
-		{
-			Vectors::TVec3<uint8_t> channels;
-		};
-
-		class RgbaPixel final
-		{
-			Vectors::TVec3<uint8_t> channels;
-			uint8_t alpha;
-		};
-
-		class MonoPixel final
-		{
-			uint8_t channel;
-		};
-	}
+	
 
 	namespace Dividers
 	{
