1a447bbf98beb27d7a4bf78d75a4fd6616f1193c
Leonid Tkachenko <leon24rus@gmail.com>
2022-01-28 15:37:58 +0700

    parameters' name are changed

--- Vectors/Vectors.h
+++ Vectors/Vectors.h
@@ -14,9 +14,9 @@ namespace WAL
 			T x;
 			T y;
 
-			const TVec2 operator=(const TVec2& rhs)
+			const TVec2 operator=(const TVec2& obj)
 			{
-				return TVec2(rhs.x, rhs.y);
+				return TVec2(obj.x, obj.y);
 			}
 
 			typedef T type;
@@ -30,9 +30,9 @@ namespace WAL
 			T y;
 			T z;
 
-			const TVec3 operator=(const TVec3& rhs)
+			const TVec3 operator=(const TVec3& obj)
 			{
-				return TVec3(rhs.x, rhs.y, rhs.z);
+				return TVec3(obj.x, obj.y, obj.z);
 			}
 
 			typedef T type;
@@ -46,9 +46,9 @@ namespace WAL
 			Vec2() : x(0), y(0) {};
 			Vec2(const uint32_t x, const uint32_t y): x(x), y(y) {};
 
-			const Vec2 operator=(const Vec2& rhs)
+			const Vec2 operator=(const Vec2& obj)
 			{
-				return Vec2(rhs.x, rhs.y);
+				return Vec2(obj.x, obj.y);
 			}
 
 		};
@@ -62,9 +62,9 @@ namespace WAL
 			Vec3() : x(0), y(0), z(0) {};
 			Vec3(const uint32_t x, const uint32_t y, const uint32_t z) : x(x), y(y), z(z) {};
 
-			const Vec3 operator=(const Vec3& rhs)
+			const Vec3 operator=(const Vec3& obj)
 			{
-				return Vec3(rhs.x, rhs.y, rhs.z);
+				return Vec3(obj.x, obj.y, obj.z);
 			}
 
 		};
